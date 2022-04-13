783a9a0b3c9014fe33e8cef0e009e0f8e5871e85
Bridget Green <90144034+Bridget-Green@users.noreply.github.com>
2022-04-13 18:00:43 -0400

    Update H_WaitingForQuestionerController.java

--- final/src/main/java/com/example/finalproject/controllers/H_WaitingForQuestionerController.java
+++ final/src/main/java/com/example/finalproject/controllers/H_WaitingForQuestionerController.java
@@ -3,14 +3,20 @@ package com.example.finalproject.controllers;
 // The Controller for frame H
 
 import com.example.finalproject.User;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+//Coded by: Bridget 
 public class H_WaitingForQuestionerController extends UtilControllerBase {
+    public Label Starting;
+    public Label waiting;
     public void initialize(URL location, ResourceBundle resources) {} // Don't have to use this
     public void Callback_connectionLost(){}
     public void Callback_gameClosed(){}
 
-    public void Callback_waitingForQuestioner(User user){} // The user object of the questioner
+    public void Callback_waitingForQuestioner(User user){
+        Starting.setText("Starting.\n"+user.getUsername()+"is the questionner");
+        waiting.setText("Please wait for a question");
+    } // The user object of the questioner
 }
