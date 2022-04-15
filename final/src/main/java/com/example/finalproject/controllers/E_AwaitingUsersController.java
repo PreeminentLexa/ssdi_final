e5d443162709d556b1028e7b4f4c95f050f745c9
Lexa <90271043+PreeminentLexa@users.noreply.github.com>
2022-04-14 23:30:40 -0400

    Semi final rundown
    Just checking things over

--- final/src/main/java/com/example/finalproject/controllers/E_AwaitingUsersController.java
+++ final/src/main/java/com/example/finalproject/controllers/E_AwaitingUsersController.java
@@ -26,84 +26,18 @@ public class E_AwaitingUsersController extends UtilControllerBase {
     Label user3;
     @FXML
     Label user4;
-    @FXML
-    Label roundsLabel;
-    @FXML
-    Label roundTimeLabel;
-    //Counter to tell how many users joined
-
-    String userID1;
-    String userID2;
-    String userID3;
-    String userID4;
-
     public void initialize(URL location, ResourceBundle resources) {} // Don't have to use this
     public void Callback_connectionLost(){}
     public void Callback_gameClosed(){}
-    //set the code to the code from the server and display it
-    @FXML
+
     public void Callback_getGameCode(String code){
         codeLabel.setText(code);
     } // The game code
-
-
-    //Displays the setting on screen (Retrieved from Server)
-    @FXML
-    public void Callback_getGameSettings(GameSettings settings){
-        roundsLabel.setText(Integer.toString(settings.getiSetting("rounds")));
-        roundTimeLabel.setText(Integer.toString(settings.getiSetting("answertime")));
-
-    }
-
-
-    //When a user joins
-    @FXML
+    public void Callback_getGameSettings(GameSettings settings){} // The game settings
     public void Callback_userJoined(User user){
-        for(int i = 0; i < 4; i++){
-            if(i==0 && user1.getText()==""){
-                user1.setText(user.getUsername());
-                userID1 = user.getUID();
-                i=4;
-            }else if(i==1 && user2.getText()==""){
-                user2.setText(user.getUsername());
-                userID2 = user.getUID();
-                i=4;
-            }else if(i==2 && user3.getText()==""){
-                user3.setText(user.getUsername());
-                userID3 = user.getUID();
-                i=4;
-
-            }else if(i==3 && user4.getText()==""){
-                user4.setText(user.getUsername());
-                userID4 = user.getUID();
-                i=4;
-
-            }
-        }
-
-    } // When a user leaves
-    @FXML
-    public void Callback_userLeft(User user){
-        for(int i = 0; i < 4;i++){
-            if(i==0 && user.getUID() ==userID1){
-                user1.setText("");
-                userID1 = "";
-            }else if(i==1 && user.getUID() ==userID2){
-                user2.setText("");
-                userID2 = "";
-            }else if(i==2 && user.getUID() ==userID3){
-                user3.setText("");
-                userID3 = "";
-
-            }else if(i==3 && user.getUID() ==userID4){
-                user4.setText("");
-                userID4 = "";
-
-            }
-        }
-
-
-    }
+        user1.setText(user.getUsername());
+    } // When a user joins
+    public void Callback_userLeft(User user){} // When a user leaves
 
     //Previous Button in the Bottom Left
     @FXML
@@ -116,7 +50,6 @@ public class E_AwaitingUsersController extends UtilControllerBase {
     @FXML
     private void startButtonAction(ActionEvent event) throws IOException{
         Utility.awaitingUsers_startGame();
-
     }
 
 
