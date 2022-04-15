9bb45d5374f73b337a989b9640100f77c28292ce
Axxaan <71111397+Axxaan@users.noreply.github.com>
2022-04-15 03:13:56 -0400

    Updated M Controller with sort function that works and displays users
    Updated M Controller with sort function that works and displays users

--- final/src/main/java/com/example/finalproject/controllers/M_ScoreBoardController.java
+++ final/src/main/java/com/example/finalproject/controllers/M_ScoreBoardController.java
@@ -11,50 +11,10 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
+/**
+ * @author Azan
+ */
 public class M_ScoreBoardController extends UtilControllerBase {
-    private User[] users;
-    private int done = 0;
-    private String getScoreText(User user){
-        return (user.isHost()?"[Host] ":"[User] ")+
-                user.getUsername()+"   "+
-                user.getScore();
-    }
-    public void initialize(URL location, ResourceBundle resources){
-        User[] allUsers = User.getAllUsers();
-        users = new User[allUsers.length];
-
-
-
-
-        // Sort allUsers into users so that score is decending
-        // users[0] should be top score
-
-
-
-
-        int ticker = 1;
-        for(User user : users){
-            System.out.println(user);
-            switch(ticker){
-                case 1:
-                    user1.setText(getScoreText(user));
-                    break;
-                case 2:
-                    user2.setText(getScoreText(user));
-                    break;
-                case 3:
-                    user3.setText(getScoreText(user));
-                    break;
-                case 4:
-                    user4.setText(getScoreText(user));
-                    break;
-            }
-            ticker++;
-        }
-//        user1.setText();
-    }
-    public void Callback_connectionLost(){}
-    public void Callback_gameClosed(){}
     @FXML
     Label user1;
     @FXML
@@ -63,33 +23,55 @@ public class M_ScoreBoardController extends UtilControllerBase {
     Label user3;
     @FXML
     Label user4;
-//
-//    @FXML
-//    public void setScoreBoard(){
-//        User[] user = User.getAllUsers();
-//        int n = user.length;
-//        int temp = 0;
-//        for(int i=0; i < n; i++){
-//            for(int j=1; j < (n-i); j++){
-//                if(user[j-1].getScore() > user[j].getScore()){
-//                    //swap elements
-//                    temp = user[j-1].getScore();
-//                    user[j-1].setScore(user[j].getScore());
-//                    user[j].setScore(temp);
-//                }
-//
-//            }
-//        }
-//        user1.setText(Integer.toString(user[0].getScore()));
-//        user2.setText(Integer.toString(user[1].getScore()));
-//        user3.setText(Integer.toString(user[2].getScore()));
-//        user4.setText(Integer.toString(user[3].getScore()));
-//
-//
-//    }
-//
-//    public void nextButtonAction(ActionEvent actionEvent) {
-//    }
+    private User[] users;
+    private int done = 0;
+    @FXML
+    private String getScoreText(User user){
+        return (user.isHost()?"[Host] ":"[User] ")+
+                user.getUsername()+"   "+
+                user.getScore();
+    }
+    @FXML
+    public void initialize(URL location, ResourceBundle resources){
+        User[] allUsers = User.getAllUsers();
+        users = new User[allUsers.length];
+
+        //test scores
+//        allUsers[0].setScore(5);
+//        allUsers[1].setScore(2);
+//        allUsers[2].setScore(3);
+
+
+        int n = allUsers.length;
+        int temp = 0;
+        // Sort allUsers into users so that score is decending
+        for(int i=0; i < n-1; i++){
+            for(int j=0; j < (n-i-1); j++){
+                if(allUsers[j].getScore() < allUsers[j+1].getScore()){
+                    //swap elements
+                    temp = allUsers[j].getScore();
+                    allUsers[j].setScore(allUsers[j+1].getScore());
+                    allUsers[j+1].setScore(temp);
+                }
+
+            }
+        }
+        for(int i = 0;i<n;i++){
+            if(i==0){
+                user1.setText(getScoreText(allUsers[i]));
+            }else if(i==1){
+                user2.setText(getScoreText(allUsers[i]));
+            }else if(i==2){
+                user3.setText(getScoreText(allUsers[i]));
+            }else if(i==4){
+                user4.setText(getScoreText(allUsers[i]));
+            }
+        }
+
+    }
+    public void Callback_connectionLost(){}
+    public void Callback_gameClosed(){}
+
 
     // This page has Utility.timeOnScoreboardPage seconds to animate (I set it to 10 seconds originally)
 }
